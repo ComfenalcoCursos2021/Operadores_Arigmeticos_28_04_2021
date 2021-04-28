48a81922cc194c7883775518b4367c50cbea4589
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-04-28 17:01:17 -0500

    Operadores Aritméticos 28-04-2021

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+    echo "<h1>Operadores Aritméticos <a href='https://www.php.net/manual/es/language.operators.arithmetic.php' target='_black'>Pagina</a> </h1>";
+
+    // $i = (int) 0;
+    // $x = (int) 5;
+
+    // Operador Post-Decremento
+    // $x--;
+    // echo $x;
+    // Operador Pre-Decremento
+    // echo --$x;
+
+    // Operador Post-Incremento
+    // $i++;
+    // echo $i;
+    // Operador Pre-Incremento
+    // echo --$i;
+
+
+    // $num1 = (int) 10;
+    // $num2 = (int) 3;
+    // Ejemplos
+
+    // echo ($num1 + $num2) + (($num1 - $num2) + 2) * 10;
+    // echo $num1 + $num2 * 2;
+    // echo "<br>";
+    // echo ($num1 + $num2) * 2;
+    // echo "<br>";
+    // echo (1-($num1 + $num2)) * 2;
+    // echo "<br>";
+    // echo 2 * $num1 + $num2;
+
+    
+    // Operador Potencia
+    // $resultado = (int) $num1 ** $num2;
+    // echo $resultado;
+    //Abrebiado 
+    // $num1 **= $num2;
+    // echo $num1;
+
+
+    // Operador Modulo
+    // $resultado = (int) $num1 % $num2;
+    // echo $resultado;
+    // Abrebiado
+    // $num1 %= $num2;
+    // echo $num1;
+
+
+    // Operador Division
+    // $resultado = (int) $num1 / $num2;
+    // echo $resultado;
+    // Abrebiado 
+    // $num1 /= $num2;
+    // echo $num1;
+
+    // Operador Multiplicacion
+    // Significado 
+    // $resultado = (int) $num1 * $num2;
+    // echo $resultado;
+    // Abrebiado 
+    // $num1 *= $num2;
+    // echo $num1;
+
+    // Operador Resta
+    // Significado 
+    // $resultado = (int) $num1 - $num2;
+    // echo $resultado;
+    // Abrebiado
+    // $num1 -= $num2;
+    // echo $num1;
+
+    // Operador Suma
+    // Significado 
+    // $resultadoDeLaSuma = (int) $num1 + $num2;
+    // echo $resultadoDeLaSuma; 
+    // Abrebiado
+    // $num1 += $num2;
+    // echo $num1;
+
+
+?>
